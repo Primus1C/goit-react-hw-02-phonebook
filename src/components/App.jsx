ada087a08cd2a833c7ea0ea022ba0d7c5ba3c603
Artur <110535829+Primus1C@users.noreply.github.com>
2023-08-20 18:05:38 +0200

    develop

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -12,16 +12,9 @@ export class App extends React.Component {
     {id: 'id-3', name: 'Eden Clements', number: '645-17-79'},
     {id: 'id-4', name: 'Annie Copeland', number: '227-91-26'},
   ],
-  filter: '',
-  name: '',
-  number: ''
+  filter: ''
 }
-
-  createContact = (data) => { 
-    console.log(data);
-  }
-  
-  
+   
   handleSubmit = evt => { 
       evt.preventDefault();
       const { name, contact } = evt.target.elements;
